abafc83829f7a25e8b7869e3a47f20ba26924330
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2021-09-14 19:22:32 -0300

    Add dist

--- src/meta.js
+++ src/meta.js
@@ -7,6 +7,7 @@
 // @run-at       document-end
 // @version      process.env.VERSION
 // @author       process.env.AUTHOR
+// @website      https://github.com/emiliopedrollo/ghubscore/
 // @require      https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.5
 // ==/UserScript==
 
